49bf2f17e3ea8c400e83a7b2a0b3945b8ba2e1a2
rokydas <droky136@gmail.com>
2020-10-03 21:18:48 +0600

    firebase config in gitignore

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,6 +3,7 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
+import { firebaseConfig } from './firebase.config';
 
 const Login = () => {
 
@@ -12,16 +13,6 @@ const Login = () => {
     const location = useLocation();
 
     let { from } = location.state || { from: { pathname: "/" } };
-
-    const firebaseConfig = {
-        apiKey: "AIzaSyA8O4D-ayKuTW7ezIurHoDMF5E-AhDwbdk",
-        authDomain: "volunteer-network18.firebaseapp.com",
-        databaseURL: "https://volunteer-network18.firebaseio.com",
-        projectId: "volunteer-network18",
-        storageBucket: "volunteer-network18.appspot.com",
-        messagingSenderId: "441128051602",
-        appId: "1:441128051602:web:c9cf0121b8a6043724974b"
-    };
 
     if (!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig);
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA8O4D-ayKuTW7ezIurHoDMF5E-AhDwbdk",
+    authDomain: "volunteer-network18.firebaseapp.com",
+    databaseURL: "https://volunteer-network18.firebaseio.com",
+    projectId: "volunteer-network18",
+    storageBucket: "volunteer-network18.appspot.com",
+    messagingSenderId: "441128051602",
+    appId: "1:441128051602:web:c9cf0121b8a6043724974b"
+};
