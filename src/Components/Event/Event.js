cf7e1ba70db83bd21e2630fb7541ebebd95663f3
rokydas <droky136@gmail.com>
2020-11-04 20:09:26 +0600

    row and column added in event and something updated

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -3,19 +3,18 @@ import './Event.css';
 
 const Event = (props) => {
     return (
-
-        <div className="card event-block" style={{width: '20rem', display: 'inline-block'}}>
-            <img className="card-img-top" src={props.event.img} alt="Card image cap"></img>
-            <div className="card-body">
-                <h5 className="card-title">{props.event.name}</h5>
-                <p className="card-text">{props.event.description}</p>
-            </div>
-            <div className="card-footer">
-                <a href={`/RegisterVolunteer/${props.event._id}`} className="btn btn-primary">Register</a>
+        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+            <div className="card event-block">
+                <img width="100%" className="card-img-top" src={props.event.img} alt="Card image cap"></img>
+                <div className="card-body">
+                    <h5 className="card-title">{props.event.name}</h5>
+                    <p className="card-text">{props.event.description}</p>
+                </div>
+                <div className="card-footer">
+                    <a href={`/RegisterVolunteer/${props.event._id}`} className="btn btn-primary">Register</a>
+                </div>
             </div>
         </div>
-        
-        
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 // import eventData from '../fakeData/eventData';
 import Event from '../Event/Event';
 import './Home.css';
+import loading from '../../images/loading.gif';
 
 const Home = () => {
 
@@ -9,15 +10,26 @@ const Home = () => {
 
     useEffect(() => {
         fetch('https://volunteer-network18.herokuapp.com/event')
-        .then(res => res.json())
-        .then(data => setEventData(data))
-    }, [])  
+            .then(res => res.json())
+            .then(data => setEventData(data))
+    }, [])
 
     return (
-        <div className="custom-container"><br/>
-            <h1 className="text-center">I grow by helping people in need.</h1><br/>
-            {
-                eventData.map(event => <Event key={event._id} event={event}></Event>)
+        <div className="custom-container"><br />
+            <h1 className="text-center">I grow by helping people in need.</h1><br />
+            {eventData.length > 0 ?
+                <div className="row">
+                    <div className="card-deck">
+                        {
+                            eventData.map(event => <Event key={event._id} event={event}></Event>)
+                        }
+                    </div>
+                </div>
+
+                :
+                <div className="text-center">
+                    <img src={loading} alt="" />
+                </div>
             }
         </div>
     );
